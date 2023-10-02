569dbf442eb04b79c0b97c44f107b6eb9336e736
Laith banat <laithahmad201092@gmail.com>
2023-10-02 16:42:27 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = function() {
+    return 2000 / 100;
+  }
+  
+  const square = (num) => {
+    return num * num;
+  }
+  
+  const add = (a, b) => {
+    return a + b;
+  }
+  
+  module.exports = {
+    divide,
+    square,
+    add,
+  };
+  
